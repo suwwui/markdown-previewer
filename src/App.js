b4f7d7080a11c05a627830deabd4814bfdc4bd30
Fadhlina Wjedi <128072211+suwwui@users.noreply.github.com>
2023-11-27 12:01:59 +0800

    preview-feat

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
 import './App.css';
+import { useState } from 'react';
+import { marked } from 'marked';
+
+// a header (H1 size), a sub header (H2 size), a link, 
+// inline code, a code block, a list item, 
+// a blockquote, an image, and bolded text
 
 function App() {
+
+  const [text, setText] = useState(`
+  # H1
+  ## H2
+  [title](https://www.example.com)
+  
+  \`code\`
+
+  \`\`\`
+{
+  "firstName": "John",
+  "lastName": "Smith",
+  "age": 25
+}
+\`\`\`
+
+- First item
+- Second item
+- Third item
+
+> blockquote
+
+![alt text](image.jpg)
+
+**bold text**
+
+`)
+
+  marked.setOptions({
+    breaks: true
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea id='editor' onChange={(event) => { setText(event.target.value) }} value={text}></textarea>
+      <div id='preview' dangerouslySetInnerHTML={{
+        __html: marked(text),
+      }}></div>
     </div>
   );
 }
